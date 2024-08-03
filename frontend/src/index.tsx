4b2d3fb1345199fe5d96af6e2867962bb4149c0d
jiheunkim <ziheun54@gmail.com>
2024-08-04 04:51:18 +0900

    [Chore 🚀] Prettier 규칙 적용

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { RouterProvider } from 'react-router-dom'
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 import 'react-pdf/dist/esm/Page/TextLayer.css'
 
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { RouterProvider } from 'react-router-dom'
 import router from './router'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)

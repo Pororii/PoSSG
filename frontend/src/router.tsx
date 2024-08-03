544bbe3c10d77a32a80b6716a3accc7ee0a31d55
jiheunkim <ziheun54@gmail.com>
2024-08-04 04:26:00 +0900

    [Design 🎨] 404 페이지 구현

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
 import Home from './pages/HomePage/Home'
+import NotFound from './components/NotFoundPage/NotFound'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '',

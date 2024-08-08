6a10bc2182cf87bf1b433a567d808cb3849f4490
jiheunkim <ziheun54@gmail.com>
2024-08-08 13:18:00 +0900

    [Feat 💡] 직무 추천 생성 및 반환으로 분리

--- frontend/src/pages/PortfolioPage/components/Recommend.tsx
+++ frontend/src/pages/PortfolioPage/components/Recommend.tsx
@@ -2,17 +2,28 @@ import { Button } from 'flowbite-react'
 import { useEffect, useState } from 'react'
 import { FaSpinner } from 'react-icons/fa'
 
+import { getAskRecommend } from '../../../api/portfolio/getAskRecommend'
 import { getRecommend } from '../../../api/portfolio/getRecommend'
 
 const Recommend = () => {
   const token = localStorage.getItem('token')
   const [recommend, setRecommend] = useState<string[]>()
-  const [, setIsRecommendLoading] = useState<boolean>(false) // 직무 추천 로딩 상태 추가
+  const [isRecommendLoading, setIsRecommendLoading] = useState<boolean>(false) // 직무 추천 로딩 상태 추가
 
   const fetchRecommend = async () => {
-    setIsRecommendLoading(true)
     if (token) {
       const successResponse = await getRecommend(token)
+      if (successResponse && successResponse.data) {
+        setRecommend(successResponse.data.message.split('직무:').slice(1))
+        console.log(successResponse.data.message)
+      }
+    }
+  }
+
+  const askRecommend = async () => {
+    setIsRecommendLoading(true)
+    if (token) {
+      const successResponse = await getAskRecommend(token)
       if (successResponse && successResponse.data) {
         setRecommend(successResponse.data.message.split('직무:').slice(1))
         console.log(successResponse.data.message)
@@ -65,10 +76,21 @@ const Recommend = () => {
                   <br />
                   <br />
                   <div className='flex justify-center items-center pb-20'>
-                    <Button className='tracking-tighter border-none font-bold px-3 bg-blue-600 hover:bg-blue-700'>
-                      <FaSpinner className='animate-spin w-8 h-8 text-white py-2 mr-1' />
-                      <p className='text-base py-2'>Please wait a moment!</p>
-                    </Button>
+                    {isRecommendLoading ? (
+                      <Button className='tracking-tighter border-none font-bold px-3 bg-blue-600 hover:bg-blue-700'>
+                        <FaSpinner className='animate-spin w-8 h-8 text-white py-2 mr-1' />
+                        <p className='text-base py-2'>Please wait a moment!</p>
+                      </Button>
+                    ) : (
+                      <Button
+                        className='tracking-tighter border-none font-bold px-3 bg-blue-600 hover:bg-blue-700'
+                        onClick={askRecommend}
+                      >
+                        <p className='text-base py-2'>
+                          Getting Job Recommendations
+                        </p>
+                      </Button>
+                    )}
                   </div>
                 </div>
               </div>
